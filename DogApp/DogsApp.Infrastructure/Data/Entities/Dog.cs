29cd83d152deb055f83423678a1c58db8e9a9473
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-10-27 09:05:59 +0300

    Upload DogsApp.Infrastructure, Entities and Class Dog

---
+++ DogApp/DogsApp.Infrastructure/Data/Entities/Dog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsApp.Infrastructure.Data.Entities
+{
+    public class Dog
+    {
+    }
+}
